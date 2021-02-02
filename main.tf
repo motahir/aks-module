c9bd217a075a707f21e1270ffa2cd4749ec98e81
Mo Tahir <mtahir@talk21.com>
2021-02-02 02:08:49 +0000

    .

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "aks_node_pool" {
-  name                  = "${var.name}-${var.env}"
+  name                  = "${var.name}${var.env}"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
   vm_size               = "Standard_DS2_v2"
   node_count            = 1
